46bc67a5cf2ad2ce6db554676ca2a62601a9f048
grupo001efg <163312970+grupo001efg@users.noreply.github.com>
2024-09-18 09:46:02 -0300

    scriptnetflix.003

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 const loginModal = document.getElementById('loginModal');
 const loginButton = document.getElementById('loginButton');
 const closeModal = document.getElementsByClassName('close')[0];
+
+loginButton.onclick = function() {
+    loginModal.style.display = 'block';
+}
+
+closeModal.onclick = function() {
+    loginModal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target === loginModal) {
+        loginModal.style.display = 'none';
+    }
+}
